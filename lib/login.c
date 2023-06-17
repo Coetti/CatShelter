6d2b8cef0ce98f5bae1e2d6a11a24bbdd4ec076d
Coetti <coettigabriel6@gmail.com>
2023-06-16 21:19:29 -0300

    add header

--- lib/login.c
+++ lib/login.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "header.h"
 
 void login(){
     char username[60];
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 /* gcc -Wall src/main.c lib/login.c lib/adminPanel.c lib/menu.c -Iinclude -o catshelter */
-#include <stdio.h>
+#include <stdio.h>"
+#include "header.h"
 
 int main(){
 
